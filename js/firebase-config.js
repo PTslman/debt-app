7e1d4c950525678e288c0300dc270831e8e82a9d
PT slman <slman2233445566y@gmail.com>
2026-08-05 23:46:05 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSQD0eam2rAczlUqnV4zIUjYey1Yyic_I",
-  authDomain: "slx23m.firebaseapp.com",
-  projectId: "slx23m",
-  storageBucket: "slx23m.firebasestorage.app",
-  messagingSenderId: "903745007698",
-  appId: "1:903745007698:web:2c1aa9ab9aed95ad2eaf8b",
-  measurementId: "G-71BB42PCEF"
+    apiKey: "AIzaSyBSQD0eam2rAczlUqnV4zIUjYey1Yyic_I",
+    authDomain: "slx23m.firebaseapp.com",
+    projectId: "slx23m",
+    storageBucket: "slx23m.firebasestorage.app",
+    messagingSenderId: "903745007698",
+    appId: "1:903745007698:web:2c1aa9ab9aed95ad2eaf8b",
+    measurementId: "G-71BB42PCEF"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
+
+console.log('🔥 Firebase initialized');
